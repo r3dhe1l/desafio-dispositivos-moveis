d685754cb9b4f7f5463c0d7f39db73af5dcd7248
Cezar Eduardo Favin MIlani <cezarefmilani@hotmail.com>
2023-09-14 19:01:13 -0300

    Adicionada a funcionalidade de tirar fotos

--- src/app/pag-solicitacao/pag-solicitacao.page.ts
+++ src/app/pag-solicitacao/pag-solicitacao.page.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Solicitacao } from 'src/model/estruturas';
-import { ToastController } from '@ionic/angular'; // Certifique-se de importar o ToastController
+import { ToastController } from '@ionic/angular';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-pag-solicitacao',
@@ -12,21 +13,21 @@ export class PagSolicitacaoPage {
 
   pedido: Solicitacao;
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private toastController: ToastController // Injete o ToastController
-  ) {
+  constructor(private route: ActivatedRoute, private router: Router, private toastController: ToastController, public photoService: PhotoService) {
     this.pedido = new Solicitacao();
     this.route.params.subscribe(params => {
-      this.pedido.tipoServico = params['tipoServ']; // Recuperar a informação de qual serviço foi selecionado
+      this.pedido.tipoServico = params['tipoServ'];
     });
+  }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
   }
 
   async enviarSolicitacao() {
     this.pedido.dataSolicitacao = new Date();
     console.log('deu certo', this.pedido.protocolo, this.pedido.tipoServico, this.pedido.detalhes, this.pedido.cep, this.pedido.endereco, this.pedido.numero, this.pedido.pontoReferencia, this.pedido.dataSolicitacao);
-    
+
     const toast = await this.toastController.create({
       message: 'Solicitação Enviada',
       duration: 5000,
