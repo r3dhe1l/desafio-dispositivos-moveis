b22faa2b484e232d0cdad482bb6936b177c250a5
Cezar Eduardo Favin MIlani <cezarefmilani@hotmail.com>
2023-09-14 18:26:46 -0300

    Adicionada mensagem de sucesso de envio da solicitação

--- src/app/pag-solicitacao/pag-solicitacao.page.ts
+++ src/app/pag-solicitacao/pag-solicitacao.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Solicitacao } from 'src/model/estruturas';
+import { ToastController } from '@ionic/angular'; // Certifique-se de importar o ToastController
 
 @Component({
   selector: 'app-pag-solicitacao',
@@ -11,13 +12,29 @@ export class PagSolicitacaoPage {
 
   pedido: Solicitacao;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastController: ToastController // Injete o ToastController
+  ) {
     this.pedido = new Solicitacao();
-    this.route.params.subscribe(params => { this.pedido.tipoServico = params['tipoServ'] }); //Recuperar a informação de qual serviço foi selecionado
+    this.route.params.subscribe(params => {
+      this.pedido.tipoServico = params['tipoServ']; // Recuperar a informação de qual serviço foi selecionado
+    });
   }
 
-  enviarSolicitacao() {
+  async enviarSolicitacao() {
     this.pedido.dataSolicitacao = new Date();
     console.log('deu certo', this.pedido.protocolo, this.pedido.tipoServico, this.pedido.detalhes, this.pedido.cep, this.pedido.endereco, this.pedido.numero, this.pedido.pontoReferencia, this.pedido.dataSolicitacao);
+    
+    const toast = await this.toastController.create({
+      message: 'Solicitação Enviada',
+      duration: 5000,
+      position: 'middle',
+      cssClass: 'custom-toast',
+    });
+    toast.present();
+
+    this.router.navigate(['/tabs/tab1']);
   }
 }
