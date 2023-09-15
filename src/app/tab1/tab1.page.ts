22515e6b520596c985ef10b3237adc591eb8f7f8
aisl1n <aislanarchy@gmail.com>
2023-09-14 22:16:41 -0300

    Alterações de estilos na tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -4,13 +4,40 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  tipoServicos = [
+    {
+      nome: 'Casa e moradia',
+      descricao: 'Iluminação pública, lixo e calçadas.',
+      icone: 'home-outline',
+    },
+    {
+      nome: 'Calçadas, ruas e estradas',
+      descricao: 'Erosão, buracos e sinalização.',
+      icone: 'earth-outline',
+    },
+    {
+      nome: 'Água e Esgoto',
+      descricao: 'Saneamento básico.',
+      icone: 'water-outline',
+    },
+    {
+      nome: 'Sinalização Urbana',
+      descricao: 'Sinalização de trânsito e placas de rua.',
+      icone: 'car-sport-outline',
+    },
+    {
+      nome: 'Animais perigosos',
+      descricao: 'Animais soltos e animais peçonhentos.',
+      icone: 'paw-outline',
+    },
+  ];
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
-  irPagSolicitacao(tipoServ: string){
+  irPagSolicitacao(tipoServ: string) {
     this.router.navigate(['/pag-solicitacao', tipoServ]);
   }
 }
